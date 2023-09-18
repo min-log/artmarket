c53f226a585a26b3aab8c2a776547ec1c378c3e1
fakey5669 <fakey5669@gmail.com>
2023-09-18 17:35:03 +0900

    상품 등록, 수정(파일 업로드 x, 카테고리, 제목, 게시글만) 완료

--- src/main/java/llustmarket/artmarket/web/controller/product/ProductController.java
+++ src/main/java/llustmarket/artmarket/web/controller/product/ProductController.java
@@ -1,7 +1,8 @@
 package llustmarket.artmarket.web.controller.product;
 
 import llustmarket.artmarket.domain.product.Product;
-import llustmarket.artmarket.web.dto.product.ProductRequestDTO;
+import llustmarket.artmarket.web.dto.product.ProductRegisterDTO;
+import llustmarket.artmarket.web.dto.product.ProductUpdateDTO;
 import llustmarket.artmarket.web.service.product.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +28,7 @@ public class ProductController {
     private ProductService productService;
 
     @PostMapping("/mypage-articles-in")
-    public ResponseEntity<?> productProcess(@RequestBody @Valid ProductRequestDTO productRequest, BindingResult bindingResult) {
+    public ResponseEntity<?> productRegisterProcess(@RequestBody @Valid ProductRegisterDTO productRequest, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             List<Map<String, String>> productRegisterErrors = new ArrayList<>();
 
@@ -63,7 +65,35 @@ public class ProductController {
                 productRegisterErrors.add(errorMap);
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(productRegisterErrors);
             }
-        } else if (productRequest.getArticleModProductId() != null && productRequest.getArticleModCategory() != null && productRequest.getArticleModTitle() != null && productRequest.getArticleModDetail() != null) {
+        }
+        List<Map<String, String>> productErrors = new ArrayList<>();
+        String fieldName = "상품 등록 정보 오류";
+        String errorMessage = "상품 등록에 필요한 정보를 모두 입력해주세요.";
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("productErrorParam", fieldName);
+        errorMap.put("productErrorMsg", errorMessage);
+        productErrors.add(errorMap);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(productErrors);
+    }
+
+    @PatchMapping("/mypage-articles-in")
+    public ResponseEntity<?> productUpdateProcess(@RequestBody @Valid ProductUpdateDTO productRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            List<Map<String, String>> productUpdateErrors = new ArrayList<>();
+
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                String fieldName = error.getField();
+                String errorMessage = error.getDefaultMessage();
+
+                Map<String, String> errorMap = new HashMap<>();
+                errorMap.put("productUpdateErrorParam", fieldName);
+                errorMap.put("productUpdateErrorMsg", errorMessage);
+                productUpdateErrors.add(errorMap);
+            }
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(productUpdateErrors);
+        }
+        if (productRequest.getArticleModProductId() != null && productRequest.getArticleModCategory() != null &&
+                productRequest.getArticleModTitle() != null && productRequest.getArticleModDetail() != null) {
             try {
                 Product product = productService.findProductByProductId(productRequest.getArticleModProductId());
                 if (product != null) {
@@ -85,21 +115,20 @@ public class ProductController {
                     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(productModErrors);
                 }
             } catch (Exception e) {
-                log.error("Product 수정 실패: {}", e.getMessage());
-                List<Map<String, String>> productModifyErrors = new ArrayList<>();
+                List<Map<String, String>> productRegisterErrors = new ArrayList<>();
                 String fieldName = String.valueOf(e.getCause());
                 String errorMessage = e.getMessage();
 
                 Map<String, String> errorMap = new HashMap<>();
-                errorMap.put("productModifyErrorParam", fieldName);
-                errorMap.put("productModifyErrorMsg", errorMessage);
-                productModifyErrors.add(errorMap);
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(productModifyErrors);
+                errorMap.put("productRegisterErrorParam", fieldName);
+                errorMap.put("productRegisterErrorMsg", errorMessage);
+                productRegisterErrors.add(errorMap);
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(productRegisterErrors);
             }
         }
         List<Map<String, String>> productErrors = new ArrayList<>();
-        String fieldName = "정보 오류";
-        String errorMessage = "정보를 모두 입력해주세요.";
+        String fieldName = "상품 수정 정보 오류";
+        String errorMessage = "상품 수정에 필요한 정보를 모두 입력해주세요.";
         Map<String, String> errorMap = new HashMap<>();
         errorMap.put("productErrorParam", fieldName);
         errorMap.put("productErrorMsg", errorMessage);
--- src/main/java/llustmarket/artmarket/web/dto/product/ProductUpdateDTO.java
+++ src/main/java/llustmarket/artmarket/web/dto/product/ProductUpdateDTO.java
@@ -3,29 +3,25 @@ package llustmarket.artmarket.web.dto.product;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
-public class ProductRequestDTO {
+public class ProductUpdateDTO {
 
-    private Long id;
-
-    private String category;
-
-    @Size(max = 30, message = "상품 제목은 최대 30자까지 입력 가능합니다.")
-    private String articleTitle;
-
-    @Size(max = 1000, message = "상품 제목은 최대 1000자까지 입력 가능합니다.")
-    private String articleDetail;
-
+    @NotNull
     private Long articleModProductId;
 
+    @NotNull
     private String articleModCategory;
 
+    @NotBlank
     @Size(max = 30, message = "상품 제목은 최대 30자까지 입력 가능합니다.")
     private String articleModTitle;
 
+    @NotBlank
     @Size(max = 1000, message = "상품 제목은 최대 1000자까지 입력 가능합니다.")
     private String articleModDetail;
 
