5ec6ebd4abf00aa3f5e6740305c83a4576cd739d
fakey5669 <fakey5669@gmail.com>
2023-09-22 13:22:45 +0900

    SecurityConfig 모든 url허용 및 로그아웃 시큐리티동작으로 변환

--- src/main/java/llustmarket/artmarket/config/SecurityConfig.java
+++ src/main/java/llustmarket/artmarket/config/SecurityConfig.java
@@ -4,11 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 @Log4j2
 @Configuration
@@ -22,11 +23,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .antMatchers("/", "/chat/*", "/apiChat", "/sub", "/topic", "/pub") //웹소켓 관련 시큐리티 제거
-                .permitAll();
+        http.authorizeRequests()
+                .antMatchers("/**", "/", "/chat/*", "/apiChat", "/sub", "/topic", "/pub").permitAll() // 웹소켓 관련 시큐리티 제거
+                .antMatchers("/logout").permitAll()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated();
+        http
+                .logout()
+                .invalidateHttpSession(true)
+                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK));
         http.csrf().disable();
-
         return http.build();
     }
 }
