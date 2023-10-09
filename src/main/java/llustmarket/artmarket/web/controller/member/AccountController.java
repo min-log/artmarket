926350d70381f9c190c7f974bfabea5c32d231e3
fakey5669 <fakey5669@gmail.com>
2023-10-09 18:49:23 +0900

    계좌정보를 바꾸는 /editMember 페이지 생성

--- src/main/java/llustmarket/artmarket/web/controller/member/AccountController.java
+++ src/main/java/llustmarket/artmarket/web/controller/member/AccountController.java
@@ -23,13 +23,26 @@ public class AccountController {
     }
 
     @PostMapping("/editMember")
-    public String editMember(@ModelAttribute Member member) {
+    public String editMember(@ModelAttribute Member member, Model model) {
         try {
-            memberService.updateAccount(member.getAccountBank(), member.getAccount(), member.getMemberId());
-            return "redirect:/editMember";
+            Member memberByMemberId = memberService.getMemberByMemberId(member.getMemberId());
+            if (memberByMemberId == null) {
+                model.addAttribute("errorMessage", "회원 아이디에 해당하는 회원이 없습니다. 정보를 확인해주세요.");
+            }
+            if (member.getAccountBank().equals("")) {
+                model.addAttribute("errorMessage", "은행명 정보를 입력해주세요.");
+            }
+            if (member.getAccount().equals("")) {
+                model.addAttribute("errorMessage", "계좌번호를 입력해주세요.");
+            }
+            if (memberByMemberId != null && !member.getAccountBank().equals("") && !member.getAccount().equals("")) {
+                memberService.updateAccount(member.getAccountBank(), member.getAccount(), member.getMemberId());
+                model.addAttribute("successMessage", "회원 정보가 성공적으로 업데이트되었습니다.");
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            return "redirect:/editMember";
+            model.addAttribute("errorMessage", "회원 정보 업데이트에 실패했습니다. 다시 시도해주세요.");
         }
+        return "editMember";
     }
 }
