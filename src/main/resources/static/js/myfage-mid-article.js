ac60410c15cd68eb8f4b6ab2d588a888b1837eb6
fakey5669 <fakey5669@gmail.com>
2023-10-11 23:12:52 +0900

    작품관리 UI수정

--- src/main/resources/static/js/myfage-mid-article.js
+++ src/main/resources/static/js/myfage-mid-article.js
@@ -30,11 +30,14 @@ function myfageArticleMidAddTag() {
           </div>
         </div>
       </div>
+      <div class="myfage-mid-article-box-top-manage-onOff-label"><strong>등록 작품</strong></div>
+      
       <div class="myfage-mid-article-box-bot"></div>
       </div>`)
 
     const myfageMidArticleBoxBot = document.querySelector('.myfage-mid-article-box-bot')
-
+    const minWidth = '850px';
+    myfageMidArticleBoxBot.style.minWidth = minWidth;
     fetch(`${baseUrl}/mypage-articles/${sessionStorage.getItem('id')}`, {
         method: 'GET',
         headers: {
@@ -44,15 +47,19 @@ function myfageArticleMidAddTag() {
         articleGetStatus = respone.status
         return respone.json()
     }).then((data) => {
-        for (var i = 0; i < data.length; i++) {
+        // 작품 데이터가 없는 경우
+        if (data.length === 0) {
+            myfageMidArticleBoxBot.insertAdjacentHTML('beforeend', `<div class="myfage-mid-article-box-bot-list">등록한 작품이 없습니다. 우측 작품 등록하기에서 작품을 등록해주세요.</div>`);
+        } else {
+            for (var i = 0; i < data.length; i++) {
 
-            data[i].articleCategory = data[i].articleCategory.toUpperCase()
+                data[i].articleCategory = data[i].articleCategory.toUpperCase()
 
-            if (data[i].articleCategory === 'LIVE') {
-                data[i].articleCategory = 'LIVE2D•3D'
-            }
+                if (data[i].articleCategory === 'LIVE') {
+                    data[i].articleCategory = 'LIVE2D•3D'
+                }
 
-            myfageMidArticleBoxBot.insertAdjacentHTML('beforeend', `<div class="myfage-mid-article-box-bot-list" data-index="${i}" id="${data[i].articleDate}">
+                myfageMidArticleBoxBot.insertAdjacentHTML('beforeend', `<div class="myfage-mid-article-box-bot-list" data-index="${i}" id="${data[i].articleDate}">
         <div class="myfage-mid-article-box-bot-list-thumb">
           <img class="myfage-mid-article-box-bot-list-thumb-img" src="data:image/jpeg;base64,${data[i].articleImgs[0]}"/>
           <img class="myfage-mid-article-box-bot-list-thumb-img" src="data:image/jpeg;base64,${data[i].articleImgs[1]}"/>
@@ -83,6 +90,7 @@ function myfageArticleMidAddTag() {
           </div>
         </div>
     </div>`)
+            }
         }
         myfageDeleteArticle();
         myfageModifyArticle();
